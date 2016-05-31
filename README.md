e3915b9bbc86b275a59f7a98de92155012486f9f
Laura Steadman <laura@quickleft.com>
2016-05-31 14:40:06 -0600

    react native maps testing

---
+++ README.md
@@ -0,0 +1,6 @@
+Getting runnign with react-native-maps:
+
+1. `npm install --save react-native-maps`
+2. `npm install -g rnpm`
+3. `rnpm link`
+4. Make sure you restart the dev server so that the new package takes effect.
---
+++ index.ios.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+class MapDemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text>Hello, Map</Text>
+        <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+AppRegistry.registerComponent('MapDemo', () => MapDemo);
